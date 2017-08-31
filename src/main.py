96cdc8ee10b9a9be2ce08f1144b656a218ad58d6
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-08-31 16:48:07 -0700

    Started working on reading the name list and storing the names in a dictionary

--- src/main.py
+++ src/main.py
@@ -4,5 +4,27 @@ Created on Aug 31, 2017
 @author: jlearx
 '''
 
+def PrintDict(dictionary):
+    for pair in dictionary.items():
+        print(pair)
+
+def ProcessNameLine(nameDict, line):
+    
+    if (line in nameDict):
+        count = nameDict[line]
+        nameDict[line] = ++count
+    else:
+        nameDict[line] = 1
+
 if __name__ == '__main__':
-    pass
+    nameFile = "../nameslist.txt"
+    nameDict = {}
+    
+    with open(nameFile, 'r') as open_file:
+        line = open_file.readline()
+        
+        while line:
+            ProcessNameLine(nameDict, line)
+            line = open_file.readline()
+    
+    PrintDict(nameDict)
