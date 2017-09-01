24df073c5d115246f93e2d398c9b98c5c4cc61cc
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-09-01 10:56:28 -0700

    Added parsing and counting of sundb file

--- src/main.py
+++ src/main.py
@@ -8,24 +8,41 @@ def PrintDict(dictionary):
     for pair in dictionary.items():
         print(pair)
 
-def ProcessNameLine(nameDict, line):
-    
-    if (line in nameDict):
-        count = nameDict[line] + 1
-        nameDict[line] = count
+def CountItem(dictionary, item):
+    if (item in dictionary):
+        count = dictionary[item] + 1
+        dictionary[item] = count
     else:
-        nameDict[line] = 1
+        dictionary[item] = 1
 
 if __name__ == '__main__':
     nameFile = "../nameslist.txt"
+    sundbFile = "../Training_01.txt"
     nameDict = {}
+    sundbDict = {}
     
+    # Read and parse the name file
     with open(nameFile, 'r') as open_file:
         line = open_file.readline()
         
         while line:
             line = line.rstrip('\n')
-            ProcessNameLine(nameDict, line)
+            CountItem(nameDict, line)
             line = open_file.readline()
     
     PrintDict(nameDict)
+    
+    # Read and parse the sundb file
+    with open(sundbFile, 'r') as open_file:
+        line = open_file.readline()
+        
+        while line:
+            line = line.rstrip('\n')
+            category = line.split('/')[2]
+            CountItem(sundbDict, category)
+            line = open_file.readline()
+    
+    PrintDict(sundbDict)
+    
+    
+    
